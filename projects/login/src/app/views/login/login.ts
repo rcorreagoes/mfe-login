d35c4cb225dcbad3d1da947c950003a94522b711
rcorreagoes <rcorreagoes@gmail.com>
2025-11-22 14:18:55 +0000

    Login

--- projects/login/src/app/views/login/login.ts
+++ projects/login/src/app/views/login/login.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  hide = true;
+}
